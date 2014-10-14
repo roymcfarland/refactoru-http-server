70833874d15c2228bb37e7254d1628b16479f3c0
Roy McFarland <roy.mcfarland@gmail.com>
2014-10-13 21:51:03 -0600

    edited with squished bugs

--- app.js
+++ app.js
@@ -1,36 +1,30 @@
 // Start the application by using "node app.js"
 
-// Use the HTTP core module to help with
-// server-based management
+// Use the HTTP core module to help with server side management
 var http = require('http');
 
 /**
- * Handler for incoming requests. Much like
- * the event handlers we are used to in jQuery,
- * but these will be called whenever any request
- * to this IP:Port is made.
- * @param  {object} request  Information about the request
- *                           Often seen as "req" instead of "request"
- * @param  {object} response Helper for sending back a response
- *                           Often seen as "res" instead of response
+ * Handler for incoming requests - like the event handlers in jQuery
+ * @param  {object} req  Information about the request
+ * @param  {object} res Helper for sending back a response
  */
-var handleRequests = function(request, response){
+var handleRequests = function(req, res){
 	console.log('Request came in.');
 
 	// Log the url that was requested
-	console.log(request.url);
+	console.log(req.url);
 
-	// This will tell the requester (in our case,
+	// Tell the requester (i.e.,
 	// the browser) that it was 200-OK and that
 	// the content to be sent will be done as plain text
-	response.writeHead(200, {
+	res.write(200, {
 		'Content-Type': 'text/plain'
 	});
 
 	// Tell the browser that it
 	// should close the connection
 	// after recieving the data in "end"
-	response.end('Hello, you requested ' + request.url);
+	res.end('Hello, you requested ' + req.url);
 };
 
 // Create a new server and tell it to pass all
